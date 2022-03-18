714f0fedc8b42c0b2bf2a977847fac005c38e44b
GustavoSilvaFerreira <guto7626@hotmail.com>
2022-03-18 17:11:10 -0300

    feat: Implementar desativar botões e Inicializar controller

--- public/controller/js/index.js
+++ public/controller/js/index.js
@@ -2,5 +2,5 @@ import Controller from "./controller.js";
 import View from "./view.js";
 
 Controller.initialize({
-    view: new View()
+    view: new View(),
 })
--- public/controller/js/view.js
+++ public/controller/js/view.js
@@ -1,9 +1,16 @@
 export default class View {
     onLoad() {
-
+        this.changeCommandBtnsVisibility()
     }
 
-    changeCommandBtnsVisibility() {
-        
+    changeCommandBtnsVisibility(hide =true) {
+        Array.from(document.querySelectorAll('[name=command'))
+        .forEach(btn => {
+            const fn = hide ? 'add' : 'remove'
+            btn.classList[fn]('unassigned')
+            function onClickReset() {}
+            btn.onClick = onClickReset
+        })
+
     }
 }
